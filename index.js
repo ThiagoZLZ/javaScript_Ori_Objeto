3ee3ea330f65349035824e3c364cd634e045ec10
Thiago Carvalho <thiago.carvalho1991@hotmail.com>
2024-03-28 23:44:40 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-function Robo(ataque, defesa, especial){
+function Robo(nome, ataque, defesa, especial){
+    this.nome = nome;
     this.ataque = ataque;
     this.defesa = defesa;
     this.especial = especial;
+
+    console.log("O Robo " + this.nome + ", tem o ataque de " + this.ataque + " e defesa de " + this.defesa + ", seu especial é " + this.especial + ".");
+    console.log("####################################################################################################################")
    
 }
 
-function RoboVoador(ataque, defesa, especial, alturaMaxima){
-    Robo.call(this, ataque, defesa, especial);
+function RoboVoador(nome, ataque, defesa, especial, alturaMaxima){
+    Robo.call(this, nome, ataque, defesa, especial);
     this.alturaMaxima = alturaMaxima;
 }  
 
-function RoboTerrestre(ataque, defesa, especial, velocidadeMaxima){
-    Robo.call(this, ataque, defesa, especial);
+function RoboTerrestre(nome, ataque, defesa, especial, velocidadeMaxima){
+    Robo.call(this, nome, ataque, defesa, especial);
     this.velocidadeMaxima = velocidadeMaxima;
 }  
 
-function RoboAguatico(ataque, defesa, especial, profundidadeMaxima){
-    Robo.call(this, ataque, defesa, especial);
+function RoboAguatico(nome, ataque, defesa, especial, profundidadeMaxima){
+    Robo.call(this, nome, ataque, defesa, especial);
     this.profundidadeMaxima = profundidadeMaxima;
 }  
 
-let RoboVoador1 = new RoboVoador(250, 150, "Fogo", 1500);
-let RoboTerrestre1 = new RoboTerrestre(240, 450, "Agua", 180);
-let RoboAguatico1 = new RoboAguatico(245, 300, "Agua e Fogo", 3000);
+let RoboVoador1 = new RoboVoador("Águia de Aço", 250, 150, "Fogo", 1500);
+let RoboTerrestre1 = new RoboTerrestre("Tanque de Ferro", 240, 450, "Agua", 180);
+let RoboAguatico1 = new RoboAguatico("Tubarão de Aço", 245, 300, "Agua e Fogo", 3000);
 
 console.log(RoboVoador1);
 console.log("################################");
